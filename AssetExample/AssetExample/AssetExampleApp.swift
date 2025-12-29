201a4d89e2830b94937d225d3124f059c909a6e5
Artem Novichkov <mail@artemnovichkov.com>
2025-12-29 14:38:46 +0500

    Add example project

---
+++ AssetExample/AssetExample/AssetExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssetExampleApp.swift
+//  AssetExample
+//
+//  Created by Artem Novichkov on 29.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct AssetExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
